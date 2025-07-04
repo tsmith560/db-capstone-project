6f07daada82c508b82e2729fa4912a0fee83e31e
Tiffany Smith <thirstsnake@MacBookPro.lan>
2025-07-04 12:30:28 -0400

    Add dummy data to StaffInfo to support Bookings table.

---
+++ sql/booking_system/insert_staffinfo.sql
@@ -0,0 +1,8 @@
+INSERT INTO StaffInfo (StaffID, EmpName, EmpRole, EmpSalary)
+VALUES
+(1, 'Alexei Kirilov', 'Server', 45000.00),
+(2, 'Anna Karenina', 'Server', 50000.00),
+(3, 'Nikolai Stavrogin', 'Server', 47000.00),
+(4, 'Ivan Fyodorovych', 'Chef', 42000.00),
+(5, 'Rodion Raskolnikov', 'Chef', 50000.00),
+(6, 'Sonya Marmeladov', 'Host', 41000.00);
