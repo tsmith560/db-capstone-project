806ac386ab9a02ba40958f16149994a57a41406f
Tiffany Smith <thirstsnake@MacBookPro.lan>
2025-07-04 19:53:00 -0400

    Made set of dummy phone numbers for final query in Jupyter notebook.

---
+++ sql/dummy_phonenumbers.sql
@@ -0,0 +1,75 @@
+ALTER TABLE CustomerDetails
+ADD COLUMN ContactNumber VARCHAR(20);
+
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1234'
+WHERE CustomerID = '50-245-9029';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1235'
+WHERE CustomerID = '07-369-4272';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1236'
+WHERE CustomerID = '39-459-6791';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1237'
+WHERE CustomerID = '11-253-6502';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1238'
+WHERE CustomerID = '84-300-6499';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1239'
+WHERE CustomerID = '49-449-2339';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1240'
+WHERE CustomerID = '38-206-8901';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1241'
+WHERE CustomerID = '46-324-1886';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1242'
+WHERE CustomerID = '38-206-8901';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1243'
+WHERE CustomerID = '63-784-1408';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1244'
+WHERE CustomerID = '72-073-0415';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1245'
+WHERE CustomerID = '84-031-0794';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1246'
+WHERE CustomerID = '85-164-1355';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1247'
+WHERE CustomerID = '32-395-2589';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1248'
+WHERE CustomerID = '97-850-3306';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1249'
+WHERE CustomerID = '68-435-7756';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1250'
+WHERE CustomerID = '07-280-9648';
+
+UPDATE CustomerDetails
+SET ContactNumber = '555-1251'
+WHERE CustomerID = '27-611-0020';
